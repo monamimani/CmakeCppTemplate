6af0407b5f16ba731dc36b0dcfb6e76aff7d3a71
monamimani <monamimani@gmail.com>
2024-02-22 07:12:39 +0000

    Use global property GENERATOR_IS_MULTI_CONFIG

--- CMake/BuildTypes.cmake
+++ CMake/BuildTypes.cmake
@@ -1,15 +1,19 @@
 include_guard()
 
 set(BUILD_TYPES "Debug" "Release" "RelWithDebInfo")
+
 # Set a default build type if none was specified
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
   set(CMAKE_BUILD_TYPE RelWithDebInfo CACHE STRING "Choose the type of build." FORCE)
+
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS $(BUILD_TYPES))
 endif()
 
-if(DEFINED CMAKE_CONFIGURATION_TYPES)
+get_property(BUILDING_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+
+if(BUILDING_MULTI_CONFIG)
   set(CMAKE_CONFIGURATION_TYPES $(BUILD_TYPES))
   set(CMAKE_CONFIGURATION_TYPES "${CMAKE_CONFIGURATION_TYPES}" CACHE STRING
     "Reset the configurations to what we need"
