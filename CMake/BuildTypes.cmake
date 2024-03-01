210d52dae62bc2ab806cbd86875ceffd29ea27be
monamimani <monamimani@gmail.com>
2024-03-01 01:11:46 +0000

    Can't validate multi config configuration types with configure time constructs.

--- CMake/BuildTypes.cmake
+++ CMake/BuildTypes.cmake
@@ -33,7 +33,7 @@ else()
     FORCE)
 
   # Can't validate Multi Config because it should be done at generator time.
-  if(NOT $<CONFIG:cfgs> IN_LIST BUILD_TYPES)
-    message(FATAL_ERROR "CMAKE_CONFIGURATION_TYPES is $<CONFIG:cfgs> and it must be one of ${BUILD_TYPES}")
-  endif()
+  # if(NOT $<CONFIG:cfgs> IN_LIST BUILD_TYPES)
+  # message(FATAL_ERROR "CMAKE_CONFIGURATION_TYPES is $<CONFIG:cfgs> and it must be one of ${BUILD_TYPES}")
+  # endif()
 endif()
