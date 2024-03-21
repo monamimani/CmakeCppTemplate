7d406f40c4a020c80742e19a5828279e058eca04
monamimani <monamimani@gmail.com>
2024-03-21 23:13:51 +0000

    Fixing Windows build

--- CMake/Tests.cmake
+++ CMake/Tests.cmake
@@ -5,41 +5,46 @@ if(TEMPLATE_PROJECT_BUILD_BENCHMARKS)
 endif()
 
 if(BUILD_TESTING) # or if(TEMPLATE_PROJECT_BUILD_TESTS)
-  # set(_FIND_COMPONENTS "") # To Silence Gtest dev warning
-  # find_package(GTest CONFIG REQUIRED)
-  # include(GoogleTest)
-  # unset(_FIND_COMPONENTS)
-
-  # if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  # set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  # endif()
   find_package(Catch2 3 CONFIG REQUIRED)
   include(Catch)
 
-  # block(SCOPE_FOR VARIABLES)
-  # unset(CMAKE_CXX_CPPCHECK)
-  # unset(CMAKE_CXX_CLANG_TIDY)
-  # FetchContent_Declare(
-  # fuzztest
-  # GIT_REPOSITORY https://github.com/google/fuzztest.git
-  # GIT_TAG main
-  # )
-  # FetchContent_MakeAvailable(fuzztest)
-  # endblock()
-  block(SCOPE_FOR VARIABLES)
-  unset(CMAKE_CXX_CPPCHECK)
-  unset(CMAKE_CXX_CLANG_TIDY)
-  CPMAddPackage(
-    NAME fuzztest
-    GITHUB_REPOSITORY google/fuzztest
-    GIT_TAG 951aada2ac48f3a37606718e60c264ed57bdd772
-    GIT_SHALLOW
-    SYSTEM
-    EXCLUDE_FROM_ALL
-  )
-  endblock()
+  if(LINUX)
+    # block(SCOPE_FOR VARIABLES)
+    # unset(CMAKE_CXX_CPPCHECK)
+    # unset(CMAKE_CXX_CLANG_TIDY)
+    # FetchContent_Declare(
+    # fuzztest
+    # GIT_REPOSITORY https://github.com/google/fuzztest.git
+    # GIT_TAG main
+    # )
+    # FetchContent_MakeAvailable(fuzztest)
+    # endblock()
+    block(SCOPE_FOR VARIABLES)
+    unset(CMAKE_CXX_CPPCHECK)
+    unset(CMAKE_CXX_CLANG_TIDY)
+    CPMAddPackage(
+      NAME fuzztest
+      GITHUB_REPOSITORY google/fuzztest
+      GIT_TAG 951aada2ac48f3a37606718e60c264ed57bdd772
+      GIT_SHALLOW
+      SYSTEM
+      EXCLUDE_FROM_ALL
+    )
+    endblock()
 
-  if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-  endif()
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+      set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    endif()
+  endif(MSVC)
+
+  if(MSVC)
+    set(_FIND_COMPONENTS "") # To Silence Gtest dev warning
+    find_package(GTest CONFIG REQUIRED)
+    include(GoogleTest)
+    unset(_FIND_COMPONENTS)
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+      set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+    endif()
+  endif(MSVC)
 endif()
